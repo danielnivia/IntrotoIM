f2457134c0740f8ae0a9e8ef9f858b3f16fae265
danielnivia <78901786+danielnivia@users.noreply.github.com>
2022-01-27 21:18:09 +0400

    Corrected file that was developed in class
    This is the final file that was developed in class. Uses the function draw and prints where the mouse is on the canvas.

--- Feb 1/potrait.js
+++ Feb 1/potrait.js
@@ -3,22 +3,22 @@ This version is a copy of today's class code by the professor
 It will serve as my initial commit although there might be changes
 */
 function setup() {
-  createCanvas(300,400); 
+  createCanvas(600,600); 
   background(25, 130, 80); 
   
   // draw a circle and a rectangle with the default colors
-  circle (300/2, 400/2, 50);
+  circle (width/2, height/2, 50);
   rect(10, 10, 80, 20);
   
   // change the stroke and fill
   fill(180,0,0);
   stroke(0,0,180);
-  // circle(80, 80, 80);
+  // circle(80, 80, 80); this circle is commented out so it won't be drawn
   ellipse(200, 280, 50, 80);
   
   // change the stroke color but keep the same fill
   stroke(180,0,0);
-  line( 0,0, 300, 300);
+  line( 0,0, 300, 100);
   
   let foo = radians(90);
   print(foo);                  //!!!!!!!!!!!!!!!!!!!!!!!! THIS IS VERY USEFUL!!!!!!!
@@ -27,4 +27,16 @@ function setup() {
       50, 50, // width and height
       0, radians(180)); // start and stop angle
   
+  triangle( 100, 100, 10, 200, 100, 300);
+  point(width/2, height/2);
+}
+
+let x = 0;
+function draw() {
+	// Where am I pointing?
+  print(mouseX, mouseY);
+  
+	// I can do animation!
+  circle(x, 500, 40);
+  x=x+1;
 }
