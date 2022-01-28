ffad5a65c64e0d2574d2a45ce9bf4c177c1218b0
danielnivia <78901786+danielnivia@users.noreply.github.com>
2022-01-28 23:21:30 +0400

    [new] personal code experiment
    This is my code experimenting with rotate function. Best resource to understand this function https://www.youtube.com/watch?v=o9sgjuh-CBM

--- Feb1/potrait.js
+++ Feb1/potrait.js
@@ -1,42 +1,32 @@
-/*
-This version is a copy of today's class code by the professor
-It will serve as my initial commit although there might be changes
-*/
+// this is the first version of my code, experimenting with rotate function
+// best resource to understand transformations until now: https://www.youtube.com/watch?v=o9sgjuh-CBM 
 function setup() {
-  createCanvas(600,600); 
-  background(25, 130, 80); 
-  
-  // draw a circle and a rectangle with the default colors
-  circle (width/2, height/2, 50);
-  rect(10, 10, 80, 20);
-  
-  // change the stroke and fill
-  fill(180,0,0);
-  stroke(0,0,180);
-  // circle(80, 80, 80); this circle is commented out so it won't be drawn
-  ellipse(200, 280, 50, 80);
-  
-  // change the stroke color but keep the same fill
-  stroke(180,0,0);
-  line( 0,0, 300, 100);
-  
-  let foo = radians(90);
-  print(foo);                  //!!!!!!!!!!!!!!!!!!!!!!!! THIS IS VERY USEFUL!!!!!!!
-  
-  arc(50, 55, // center of the ellipse
-      50, 50, // width and height
-      0, radians(180)); // start and stop angle
-  
-  triangle( 100, 100, 10, 200, 100, 300);
-  point(width/2, height/2);
+  createCanvas(400, 400);
 }
 
-let x = 0;
 function draw() {
-	// Where am I pointing?
-  print(mouseX, mouseY);
+  background(66, 245, 147);
+  stroke(255);
+  line(0, 200, 400, 200); // x-axis
+  line(200, 0, 200, 400); //y-axis
+  //------------------
+
+  translate(200, 200);
+  fill(50, 50, 80);
+  stroke(0);
+  //-------------
+  rotate(radians(45));
+  rect(0, 0, 50, 50);
+  //-------------
+  fill(30, 35, 180);
+  rotate(radians(-45));
+  rotate(radians(120));
+  rect(0, 0, 80, 30);
+  //-------------
+  fill(250,5,45)
+  rotate(radians(-120)) // reset to 0 degreeszoo
+  rotate(radians(280))
+  ellipse(0,0,50,60)
   
-	// I can do animation!
-  circle(x, 500, 40);
-  x=x+1;
 }
+
