84a0da8eb652800535475dbbbd9f573da55e5084
danielnivia <78901786+danielnivia@users.noreply.github.com>
2022-01-29 03:38:30 +0400

    [update] rotating ellipse around a point
    This new update uses the functions push and pop. Uses the variable angle to animate an ellipse rotating around point (300,200)

--- Feb1/potrait.js
+++ Feb1/potrait.js
@@ -1,8 +1,12 @@
 // this is the first version of my code, experimenting with rotate function
 // best resource to understand transformations until now: https://www.youtube.com/watch?v=o9sgjuh-CBM 
 // code will be an experiment from now on
+//program that rotates a circle around point (300,200)
+let angle = 0;
+
 function setup() {
   createCanvas(400, 400);
+  //rectMode(CENTER)
 }
 
 function draw() {
@@ -16,18 +20,31 @@ function draw() {
   fill(50, 50, 80);
   stroke(0);
   //-------------
-  rotate(radians(45));
+  push(); // serves as a save this temporary changes
+  rotate(radians(50));
   rect(0, 0, 50, 50);
+  pop(); // serves as restore the original settings
   //-------------
   fill(30, 35, 180);
-  rotate(radians(-45));
-  rotate(radians(120));
+  //rotate(radians(-45));
+  push();
+  rotate(radians(180));
   rect(0, 0, 80, 30);
+  pop();
   //-------------
-  fill(250,5,45)
-  rotate(radians(-120)) // reset to 0 degreeszoo
-  rotate(radians(280))
-  ellipse(0,0,50,60)
-  
+  fill(250, 5, 45);
+  //rotate(radians(-120)) // reset to 0 degreeszoo
+  translate(100, 0);
+  ellipseMode(CENTER);
+  ellipse(0, 0, 10);
+  push();
+  ellipseMode(CORNER);
+  rotate(radians(angle));
+
+  ellipse(0, 20, 40, 60);
+  line(0, 0, 0, 20);
+  angle = angle + 1;
+  pop();
 }
+
 
