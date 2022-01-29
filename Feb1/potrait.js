5746bab919cb62d999ccf8c2f36914dcf3267af8
danielnivia <78901786+danielnivia@users.noreply.github.com>
2022-01-29 14:14:01 +0400

    [update] drawing a musical note & resetMatrix func
    Cleaned up the code, eliminated unnecessary objects (except the last rectangle under resetMatrix func). Drew the musical note, there are challenges with the translation + rotation transformations to draw the object (specifically ellipse).

--- Feb1/potrait.js
+++ Feb1/potrait.js
@@ -17,44 +17,57 @@ function setup() {
 
 function draw() {
   //----
-  //----
   //setting up
   background(66, 245, 147); // turquoise background color
   stroke(255); // white lines to show the center
+  //grid for drawing around
   line(0, 200, 400, 200); // x-axis
   line(200, 0, 200, 400); //y-axis
+  //upper left cuadrant
+  line(0, 100, 400, 100);
+  line(100, 0, 100, 400);
+  //bottom left cuadrant
+  line(0, 300, 400, 300);
+  //right cuadrants
+  line(300, 0, 300, 400);
+  //----
+  fill(20,34,200)
   //----
   //----
-
+  // general adjustments
   translate(200, 200); // sets origin to (200,200)
-  fill(50, 50, 80);
-  stroke(0);
-  //-------------
-      push();
-  rotate(radians(50));
-  rect(0, 0, 50, 50);
-      pop();
+  noStroke(); // sets no stroke to all unless stated to add stroke
   //----
-  fill(30, 35, 180);
-      push();
-  rotate(radians(180));
-  rect(0, 0, 80, 30);
-      pop();
+  //TEST Rectangle
+          push();
+      fill(50, 50, 80); // greyish blue
+      rotate(radians(45)); // rotate 50 degrees from top left corner from origin (200,200)
+      strokeWeight(4) // stroke weight
+      stroke(255, 204, 0) // added stroke
+      rect(0, 0, 50, 50);
+          pop();
+  
   //----
-  fill(250, 5, 45);
-  
-  translate(100, 0);
-      push();
-  ellipseMode(CENTER); // rotates around upper left corner
-  rotate(radians(20));
-  fill(25)
-  ellipse(0, 0, 40, 60);
-      pop();
-  
-  ellipseMode(CENTER); // rotates around center of shape
-  ellipse(0, 0, 10);
-  
+  //the stem of the musical note
+          push();
+      fill(25)
+      rect(99, -50, 10, 80,5); // -50 because origin is (200,300)
+          pop();
+  //----
+  //----
+  //head of the musical note
+  translate(100, 0); // changes origin to (300,200)
+          push();
+      ellipseMode(CENTER); 
+      rotate(radians(60)); // rotate 60 degrees
+      fill(25);
+      ellipse(25, 20, 20, 30); // the rotation affects calculation
+                               //of where the ellipse goes but in
+                               //theory is 25x, 20y from (300,200) 
+          pop();
+  //----
+  //reseting the matrix [origin back to (0,0)]
+  resetMatrix();
+  rect(10,2,40,50);
+  //----
 }
-
-
-
