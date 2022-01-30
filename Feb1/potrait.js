4d28d826c0aa9684ba24c94b8db1e021dfe4af10
danielnivia <78901786+danielnivia@users.noreply.github.com>
2022-01-30 15:55:37 +0400

    [update] latest work on the iPad
    This was the code that I worked while taking the Expo bus

--- Feb1/potrait.js
+++ Feb1/potrait.js
@@ -9,64 +9,160 @@ Uses 2D primitives and some transform functions.
 */
 
 function setup() {
-  //----
-  createCanvas(400, 400);
-  // putting the background here reduces the quality of the shapes
+   //----
+   createCanvas(400, 400);
+   // putting the background here reduces the quality of the shapes
 }
 
 function draw() {
-  //----
-  //setting up
-  background(66, 245, 147); // turquoise background color
-  stroke(255); // white lines to show the center
-  //grid for drawing around
-  line(0, 200, 400, 200); // x-axis
-  line(200, 0, 200, 400); //y-axis
-  //upper left cuadrant
-  line(0, 100, 400, 100);
-  line(100, 0, 100, 400);
-  //bottom left cuadrant
-  line(0, 300, 400, 300);
-  //right cuadrants
-  line(300, 0, 300, 400);
-  //----
-  fill(20,34,200)
-  //----
-  //----
-  // general adjustments
-  translate(200, 200); // sets origin to (200,200)
-  noStroke(); // sets no stroke to all unless stated to add stroke
-  //----
-  //TEST Rectangle
-          push();
-      fill(50, 50, 80); // greyish blue
-      rotate(radians(45)); // rotate 50 degrees from top left corner from origin (200,200)
-      strokeWeight(4) // stroke weight
-      stroke(255, 204, 0) // added stroke
-      rect(0, 0, 50, 50);
-          pop();
-  
-  //----
-  //the stem of the musical note
-          push();
-      fill(25)
-      rect(99, -50, 10, 80,5); // -50 because origin is (200,300)
-          pop();
-  //----
-  //----
-  //head of the musical note
-  translate(100, 0); // changes origin to (300,200)
-          push();
-      ellipseMode(CENTER); 
-      rotate(radians(60)); // rotate 60 degrees
-      fill(25);
-      ellipse(25, 20, 20, 30); // the rotation affects calculation
-                               //of where the ellipse goes but in
-                               //theory is 25x, 20y from (300,200) 
-          pop();
-  //----
-  //reseting the matrix [origin back to (0,0)]
-  resetMatrix();
-  rect(10,2,40,50);
-  //----
+   //----
+   //----setting up----\\
+   background(66, 245, 147); // turquoise background color
+   stroke(255); // white lines to show the center
+   //grid for drawing around
+   line(0, 200, 400, 200); // x-axis
+   line(200, 0, 200, 400); //y-axis
+   //upper left cuadrant
+   line(0, 100, 400, 100);
+   line(100, 0, 100, 400);
+   //bottom left cuadrant
+   line(0, 300, 400, 300);
+   //right cuadrants
+   line(300, 0, 300, 400);
+   //---- test fill and shape ----\\
+   //   fill(20,34,200)
+   //		rect(10,10,40,30)
+   //----
+   
+   //----
+   // general adjustments
+   translate(200,150); // origin (200,150) 
+   noStroke(); // sets no stroke to all unless stated to add stroke
+   //----
+   
+   //---- FEET ----\\
+   //shoes adidas 3 stripes
+   push();
+   fill(10);
+   resetMatrix(); // reset to (0,0)
+   translate(200,350); // temporary new origin for push and pop
+   rectMode(CENTER);
+   rect(-38,30,40,15,10,0,10,5); // left shoe
+   rect(38,30,40,15,0,10,5,10); // right shoe
+   stroke(225);
+   strokeWeight(1.25);
+	//left shoe lines
+   line(-35,20,-45,38); // line 1 left foot
+   line(-30,20,-40,38); // line 2 left foot
+   line(-25,20,-35,38); // line 3 left foot
+	//right shoe lines
+   line(35,20,45,38); // line 1 right foot
+   line(30,20,40,38); // line 2 right foot
+   line(25,20,35,38); // line 3 right foot
+   pop();
+	//--------------\\
+   //---- LEGS ----\\
+   push();
+   fill(200)
+   rect(-40,120,80,40); //width of the body
+   ellipse(0,145,80,40);
+   rectMode(CENTER);
+   //-------------
+   rect(30,175,25,100,10); // legR
+   rect(-30,175,25,100,10); // legL >> this works without the reset
+   //-------------
+   
+   pop();
+   
+	//--------------\\   
+   //---- BODY and ARMS ----\\
+   //blue sandwich bleu logo in the middle
+   resetMatrix(); 
+   translate(200,200);
+   fill(240,15,20); // color of the body
+   rect(-40,-20,80,100);
+	// right arm
+
+push();
+	fill(240,240,20); // color of the arm
+rectMode(CENTER);
+	rotate(radians(45));
+	rect(60,-31,100,20);
+
+	fill(10);
+	rect(110,-31,20,20,5);
+pop();
+
+	fill(0);
+	ellipse(0,0,10)
+
+	// left arm
+push();
+	fill(240,240,20); // color of the arm
+rectMode(CENTER);
+	rotate(radians(45))
+	rect(-20,31,20,100);
+pop();
+	//--------------\\
+
+
+
+   
+   //---- FACE ----\\
+   // drawing the head
+   push();
+   resetMatrix();
+   translate(200,150);
+   ellipseMode(CENTER);
+   fill(124,20,10); // NEED TO LOOK FOR SKIN COLOR
+   ellipse(0,0,100,90);
+   pop();
+   //--
+   //--drawing the smile with teeth
+   fill(225);
+   resetMatrix(); // reset to (0,0)
+   translate(200,150); // return to (200,150) 
+   arc(0, 5, 80, 60, 0, radians(180), OPEN);
+   
+   //--teeth
+   stroke(0);
+   strokeWeight(1.25);
+   line(-35,20,35,20); // horizontal line for teeth
+   line(0,5,0,35);// middle line teeth
+   // right side
+   line(10,5,10,33);// right1
+   line(20,5,20,31);// right2
+   line(30,5,30,26);// right3
+   // left side
+   line(-10,5,-10,33);// left1
+   line(-20,5,-20,31);// left2
+   line(-30,5,-30,26);// left3	
+   
+   //--eyes
+   push();
+   noStroke(); // no keeping outerline in shapes
+   
+   //right eye
+   ellipseMode(CENTER);
+   fill(225);
+   ellipse(20,-20,20,10);
+   fill(0); // black eye
+   ellipse(20,-20,10,8);
+   //left eye
+   fill(225);
+   ellipse(-20,-20,20,10);
+   fill(0); // black eye
+   ellipse(-20,-20,10,8);
+   pop();
+   
+   //--------------\\
+   
+   
+   
+   //---- Music Notes ----\\
+// quater note
+   //the stem of the musical note
+ 
+// two eighth notes
+   //----
 }
